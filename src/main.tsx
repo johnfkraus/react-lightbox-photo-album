5faa387e200734bdf112d618fe7059f30fea210c
John Kraus <johnkraus3@gmail.com>
2025-08-10 14:59:29 -0400

    init

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <header>
+      <h1>React Photo Album | Lightbox</h1>
+      {/*<a href="https://github.com/igordanchenko/react-photo-album" target="_blank" rel="noreferrer noopener">*/}
+      {/*  GitHub*/}
+      {/*</a>*/}
+      {/*<a href="https://react-photo-album.com/" target="_blank" rel="noreferrer noopener">*/}
+      {/*  Docs*/}
+      {/*</a>*/}
+    </header>
+    <main>
+      <App />
+    </main>
+    <footer className="app-footer">
+      A thing made by John Kraus
+    </footer>
+  </React.StrictMode>,
+);
