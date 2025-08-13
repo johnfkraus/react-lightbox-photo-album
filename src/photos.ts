39036ffd18fd169cd5c10c50365e5120e4f1e4ad
John Kraus <johnkraus3@gmail.com>
2025-08-12 20:51:54 -0400

    added photos

--- src/photos.ts
+++ src/photos.ts
@@ -11,12 +11,21 @@ function imageLink(path: string, width: number, height: number, size: number, ex
 
 const photos = [
   // see 'filenames.md' for tips on how to refer to photos in this list
+  // see scripts for naming image srcsets.
 
   { src: "meeks.3684.3024x4032.jpg", alt: "Meeks" },
   { src: "bronze.3623.1493x1613.jpg", alt: "Bronze figurines" },
-  { src: "Engelhartszell_an_der_Donau_Innviertel_Austria.1000.4032x3024.jpg", alt: "Engelhartszell_an_der_Donau_Innviertel_Austria" },
+  { src: "Engelhartszell_an_der_Donau_Innviertel_Austria.1000.4032x3024.jpg",
+    alt: "Engelhartszell an_der Donau, Innviertel, Austria" },
   { src: "amery.3611.3024x4032.jpg", alt: "Amery, Wisconsin, vase, 1987" },
   { src: "birdhouse.7943.3024x4032.jpg", alt: "Birdhouse"},
+  { src: "Budapest,_Inner_City,_Central_Hungary,_Hungary.22.3024x4032.jpg",
+    alt: "Budapest, Inner City, Central Hungary, Hungary" },
+  { src: "Catedral_of_Esztergom.22.3024x4032.jpg",
+    alt: "Catedral of Esztergom, Esztergom, Komárom-Esztergom County, Hungary" },
+  { src: "English_Garden.1.3024x4032.jpg", alt: "English Garden, Munich" },
+  { src: "hello-kitty-sandals.8330.2113x2340.jpg", alt: "Hello Kitty Sandals" },
+
   // { src: "annapolis.6702.3024x4032.jpg", alt: "Sailing on a schooner in Annapolis, Maryland" },
   // { src: "bronze.3623.1493x1613.jpg", alt: "Bronze figurines" },
   // { src: "buffalo.7161.4032x3024.jpg", alt: "South Dakota buffalo" },
